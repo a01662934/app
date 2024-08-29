d959dd6a32888b8e76a73ba3092c5a280e1265c7
Aracelli <a01662934@tec.mx>
2024-08-29 17:46:23 -0600

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Aracelli Boza on 29/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
